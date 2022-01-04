660cb8b88d9dc1806580b7c63501e6bfc88724e9
jung su young <j9972@naver.com>
2022-01-04 23:30:51 +0900

    connecting with naver map api

--- src/map/NaverMap.js
+++ src/map/NaverMap.js
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
 import React from "react";
 
 function NaverMap() {
-  return <div>hello</div>;
+  const { naver } = window;
+
+  const mapOptions = {
+    center: new naver.maps.LatLng(33.386625156730965, 126.52905333401526),
+    zoom: 11,
+  };
+
+  useEffect(() => {
+    const map = new naver.maps.Map("map", mapOptions);
+  }, []);
+
+  return (
+    <div>
+      <div id="map" style={{ width: "1000px", height: "600px" }}></div>
+    </div>
+  );
 }
 
 export default NaverMap;

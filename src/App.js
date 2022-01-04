034928073cec33fad4bb90d4da71d4f8ee38a665
jung su young <j9972@naver.com>
2022-01-04 22:28:30 +0900

    make a new file of naver map and setting for coding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import NaverMapApi from "./map/NaverMap";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NaverMapApi />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/map/NaverMap.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NaverMap() {
+  return <div>hello</div>;
+}
+
+export default NaverMap;
